8df20bdb5842540f5ed59af6a5ec000206a6ae6f
Kubanychbek Tursunbekov <kubanychbek.tursunbekov.alt@gmail.com>
2025-06-17 19:44:40 +0300

    add userauths app

---
+++ userauths/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+  """Расширение модели пользователя"""
+  """User Model Extension"""
+  email = models.EmailField(unique=True)
+  username = models.CharField(max_length=100)
+  USERNAME_FIELD = "email"
+  REQUIRED_FIELDS = ["username"]
+
+  def __str__(self):
+    return self.username
+  
