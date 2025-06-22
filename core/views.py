8f98bd74652362be2ebef8615d902367fff798fd
Kubanychbek Tursunbekov <kubanychbek.tursunbekov.alt@gmail.com>
2025-06-22 22:34:22 +0300

    update

--- core/views.py
+++ core/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+# from django.db.models import Count
 from .models import Category, Vendor, Product, ProductImages, CartOrder, CartOrderItems, ProductReview, WishList, Address
 
 
 def index(request):
+  # Подсчет всех товаров этой категории
+  # categories = Category.objects.all().annotate(product_count=Count("products"))
   categories = Category.objects.all()
 
   context = {
